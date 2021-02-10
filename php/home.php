c3cd7723f34d2481a056e9c8e99fe0a4eac39fe4
Nathaniel Bedassie <nathanielbedassie@gmail.com>
2021-02-09 22:09:39 -0500

    working on tags

--- php/home.php
+++ php/home.php
@@ -12,9 +12,54 @@
         </div>
 
         <div class="tags">
-            <p>tag1</p>
-            <p>tag2</p>
-            <p>tag2</p>
+            <div class="tag">
+                <p>tag1</p>
+            </div>
+            <div class="tag">
+                <p>tag2</p>
+            </div>
+            <div class="tag">
+                <p>tag2</p>
+            </div>
+            <!--
+            <svg width="500" height="100" xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg">
+            <!-- Created with SVG-edit - https://github.com/SVG-Edit/svgedit--><!--
+            <defs>
+            <radialGradient id="svg_4" spreadMethod="pad">
+            <stop offset="0" stop-color="#000000"/>
+            <stop offset="1" stop-color="#ffffff"/>
+            </radialGradient>
+            </defs>
+            <g class="layer">
+            <title>Layer 1</title>
+            <ellipse cx="252.999998" cy="53.000001" fill="url(#svg_4)" id="svg_1" rx="216.999995" ry="15" stroke="#000000" stroke-opacity="0" stroke-width="5"/>
+            </g>
+            </svg>
+            <svg width="500" height="100" xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg">
+            <!-- Created with SVG-edit - https://github.com/SVG-Edit/svgedit-->
+            <defs>
+            <radialGradient id="svg_4" spreadMethod="pad">
+            <stop offset="0" stop-color="#000000"/>
+            <stop offset="1" stop-color="#ffffff"/>
+            </radialGradient>
+            </defs>
+            <g class="layer">
+            <title>Layer 1</title>
+            <ellipse cx="252.999998" cy="53.000001" fill="url(#svg_4)" id="svg_1" rx="216.999995" ry="15" stroke="#000000" stroke-opacity="0" stroke-width="5"/>
+            </g>
+            </svg><svg width="500" height="100" xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg">
+            <!-- Created with SVG-edit - https://github.com/SVG-Edit/svgedit-->
+            <defs>
+            <radialGradient id="svg_4" spreadMethod="pad">
+            <stop offset="0" stop-color="#000000"/>
+            <stop offset="1" stop-color="#ffffff"/>
+            </radialGradient>
+            </defs>
+            <g class="layer">
+            <title>Layer 1</title>
+            <ellipse cx="252.999998" cy="53.000001" fill="url(#svg_4)" id="svg_1" rx="216.999995" ry="15" stroke="#000000" stroke-opacity="0" stroke-width="5"/>
+            </g>
+            </svg>-->
         </div>
 
         <div class="ad">
