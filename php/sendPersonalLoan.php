f20d2a38a26215d3ae1f54919406f378aa9a353c
amoablo <ksterling542@gmail.com>
2021-06-13 13:26:05 -0500

    sending error fixed

--- php/sendPersonalLoan.php
+++ php/sendPersonalLoan.php
@@ -233,6 +233,7 @@ $mail = new PHPMailer(true);
 try {
     //Server settings
     $mail->SMTPDebug = SMTP::DEBUG_OFF;                      //Enable verbose debug output
+    //$mail->SMTPDebug = true;
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
